f95e6482f18e9cf771ebfa71cf72695f9de453e6
Rochadev14 <danielrochadev14@gmail.com>
2025-06-09 15:36:01 +0200

    Añadiendo atributos de tareas getter settters etc

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+
 public class Main {
     public static void main(String[] args) {
+    Tareas tarea1 = new Tareas( "Estudiar Java");
 
     }
 }
---
+++ Tareas.java
@@ -0,0 +1,56 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Tareas {
+    private int id =0;
+    private String descripcion;
+    private String Status;
+    private String fechaCreacion;
+    private String actualizadoen;
+
+    String aplicarFecha() {
+        LocalDateTime now = LocalDateTime.now();
+        String formatted = now.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        return formatted;
+    }
+    public Tareas(String descripcion) {
+        this.id =+1;
+        this.descripcion = descripcion;
+        Status = "todo";
+        this.fechaCreacion = aplicarFecha();
+        this.actualizadoen = fechaCreacion;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getStatus() {
+        return Status;
+    }
+    public void setStatus(String status) {
+        Status = status;
+    }
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public String getActualizadoen() {
+        return actualizadoen;
+    }
+    public void setActualizadoen(String actualizadoen) {
+        this.actualizadoen = actualizadoen;
+
+    }
+    public String list() {
+        return "ID: " + id + ", Descripcion: " + descripcion + ", Status: " + Status + ", Fecha de Creacion: " + fechaCreacion + ", Actualizado en: " + actualizadoen;
+    }
+
+}
